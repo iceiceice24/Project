85a15b3d74d80405e9b1376c33e61fe6e6d20ebd
mens-rea <juan.miguel.g.alvarez@gmail.com>
2021-10-11 14:17:49 +0800

    Secure firebase config details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
+import configData from "./firebase-config.json";
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -9,15 +10,7 @@ import 'firebase/analytics';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCy_KXNHcGDqvUfKB8C_oMHZcf_7o2r6q0",
-  authDomain: "test-36a96.firebaseapp.com",
-  projectId: "test-36a96",
-  storageBucket: "test-36a96.appspot.com",
-  messagingSenderId: "925756792414",
-  appId: "1:925756792414:web:f8f993a8f8d85362a85ecf",
-  measurementId: "G-B2Y4Q7T84R"
-})
+firebase.initializeApp(configData);
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
